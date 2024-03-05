a4b228fc56c45b0c3082543790da785b44f013b8
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-05 04:24:36 -0800

    fix pycodestyle error, and update documentation

--- models/base_model.py
+++ models/base_model.py
@@ -4,23 +4,25 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
-    BaseModel
+    Base class for all models in the AirBnB clone project.
 
     Attributes:
-       id: uuid of the model instance
-       created_at: Time of the creation
-       updated_at: Last time updated
+        id: UUID of the model instance.
+        created_at: Timestamp of when the instance was created.
+        updated_at: Timestamp of when the instance was last updated.
     """
 
     # Initiaisation
     def __init__(self, *args, **kwargs):
-        """ 
-        Initialize BaseModel instance 
-        
-           *args : Will not be used
-           **kwargs: arguments for the constructor of a BaseModel
+        """
+        Initializes a BaseModel instance.
+
+        Args:
+          *args: Unused positional arguments.
+          **kwargs: Keyword arguments for the constructor.
         """
         if kwargs:
             for key, value in kwargs.items():
@@ -36,18 +38,22 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
-
     # Public Methodes
     def __str__(self):
         """ Return string representation of BaseModel instance """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ Update the public instance attribute updated_at with the current time """
+        """ Updates the `updated_at` attribute with the current datetime. """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ Return a dictionary containing all the keys/values of __dict__ of the instance """
+        """
+        Returns a dictionary representation of the BaseModel instance.
+
+        Returns:
+          dict: dictionary containing key-value pairs of instance attributes.
+        """
         instance_dict = self.__dict__.copy()
         instance_dict['__class__'] = self.__class__.__name__
         instance_dict['created_at'] = self.created_at.isoformat()
