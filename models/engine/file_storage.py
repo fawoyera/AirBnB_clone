52a36d6f9049764abd02f478d9dcc05f2932696e
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-06 13:54:12 -0800

    .

---
+++ models/engine/file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" FileStorage Class """
+
+class FileStrorage:
+    """
+    This class will serializes instances to a JSON file and deserializes JSON file to instances
+    """
+
+    # Private Attributes
+    self.__file_path = 
