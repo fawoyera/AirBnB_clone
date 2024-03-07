7164a6a1d39f8b87ddc6839c642f5df0ee482719
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-06 17:24:55 -0800

    update the import storage in base_model to avoid ImportError due to a circular import

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -35,6 +34,8 @@ class BaseModel:
                     setattr(self, key, value)
 
         else:
+            from models import storage
+
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -47,6 +48,8 @@ class BaseModel:
 
     def save(self):
         """ Updates the `updated_at` attribute with the current datetime. """
+        from models import storage
+
         self.updated_at = datetime.now()
         storage.save()
 
