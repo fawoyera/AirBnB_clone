92961a6d8093f14429e12ecfbfb106206d1fd4f6
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-10 01:57:11 -0800

    update user class

--- models/user.py
+++ models/user.py
@@ -4,7 +4,14 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ This class defines the user modual and it attributes """
+    """ This class defines the user modual and it attributes 
+    
+    Attributes:
+      email: User email (string)
+      password: User password (string)
+      first_name: User first name (string)
+      last_name: User last name (string)
+    """
     email = ""
     password = ""
     first_name = ""
